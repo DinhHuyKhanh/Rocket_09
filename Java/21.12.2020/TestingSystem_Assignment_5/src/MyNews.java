d46b864a62c530b378bc9fcd64067f13f74157d6
DinhHuyKhanh <khanhdinh141@gmail.com>
2020-12-21 23:51:36 +0700

    core java 21/12/2020

---
+++ Java/21.12.2020/TestingSystem_Assignment_5/src/MyNews.java
@@ -0,0 +1,34 @@
+import sun.applet.Main;
+
+import java.util.ArrayList;
+
+public class MyNews {
+    //    c) Tạo chương trình demo có tên là MyNews và tạo một menu
+//    lựa chọn gồm các mục sau:
+//             Insert news
+// View list news
+// Average rate
+// Exit
+//    Nếu người dùng chọn 1 từ bàn phím thì tạo một object của
+//    class News và nhập giá trị cho các thuộc tính Title,
+//            PublishDate, Author, Content sau đó yêu cầu người dùng
+//            nhập vào 3 đánh giá để lưu vào Rates.
+//    Nếu người dùng chọn 2 từ bàn phím thì thực thi method
+//    Display().
+//    Nếu người dùng chọn 3 từ bàn phím thì thực hiện method
+//    Calculate() để tính đánh giá trung bình, sau đó thực thi
+//    method Display().
+//    Trường hợp người dùng chọn 4 thì sẽ thoát khỏi chương
+//    trình.
+
+    public static void main(String[] args){
+//        News[] news = new News[4];
+//        news[0] = new News(1, "Sơn Tùng", "1", "1", "1");
+//        news[1] = new News(2, "Sơn Tùng 1 ", "2", "2", "2");
+//        news[2] = new News(3, "Sơn Tùng 2", "3", "3", "3");
+//        news[3] = new News(4, "Sơn Tùng 3", "4", "4", "4");
+        ArrayList list = new ArrayList<News>();
+        // nhap
+
+    }
+}
